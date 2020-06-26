ca8e2755d01be15bb9d454249278690cc9603d9a
Jeroeny <spee.jeroen@gmail.com>
2020-06-26 14:47:49 +0200

    add more test cases add possible fix

--- run.php
+++ run.php
@@ -1,16 +1,31 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
-
-$regex = \Symfony\Component\Finder\Gitignore::toRegex(file_get_contents(__DIR__ . '/.gitignore'));
+require __DIR__ . '/Gitignore.php';
 
 $files = [
     'example/example.txt',
+    'example/test',
     'example/packages/file.yaml',
     'example/packages/example.yaml',
     'example/foo/bar.txt',
+    'test/bar',
+    'test/bar/',
 ];
-foreach ($files as $file) {
-    $match = preg_match($regex, $file) === 1;
-    echo $file . ' ignored: ' . var_export($match, true) . PHP_EOL;
+
+$regex = \Symfony\Component\Finder\Gitignore::toRegex(file_get_contents(__DIR__ . '/.gitignore'));
+
+test($regex, $files);
+
+$fixedRegex = \Gitignore::toRegex(file_get_contents(__DIR__ . '/.gitignore'));
+
+test($fixedRegex, $files);
+
+function test(string $pattern, array $files) {
+    echo $pattern . PHP_EOL;
+
+    foreach ($files as $file) {
+        $match = preg_match($pattern, $file) === 1;
+        echo $file . ' ignored: ' . var_export($match, true) . PHP_EOL;
+    }
 }
